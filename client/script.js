64a940813e290387e71af295d0b44ae847c49167
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2023-12-28 13:06:56 +0100

    lol

--- client/script.js
+++ client/script.js
@@ -1,7 +1,6 @@
 const appContainer = document.getElementById("app");
-let username, color;
+let username, team;
 appContainer.innerHTML = `
-
 <img src="assets/logo.png" style="width: 500px;"/><br/>
 <input type="text" id="username"/>
 <button onclick="test()">Next</button>
@@ -17,10 +16,11 @@ function test() {
     `;
 }
 
-function chooseTeam(team) {
-    color = team;
+function chooseTeam(t) {
+    team = t;
 }
 
 function play() {
-    console.log(username, color)
+    fetch("http://127.0.0.1:5000/register", {method: "POST", body: JSON.stringify({username, team}), mode: "cors"})
+    console.log(username, team)
 }
