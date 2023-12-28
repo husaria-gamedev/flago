da038a75041be899cad561568b1240eb68cba91c
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2023-12-28 14:10:26 +0100

    basic frontend

--- client/script.js
+++ client/script.js
@@ -1,18 +1,20 @@
 const appContainer = document.getElementById("app");
 let name, team;
 appContainer.innerHTML = `
-<img src="assets/logo.png" style="width: 500px;"/><br/>
-<input type="text" id="username"/>
-<button onclick="test()">Next</button>
+<img src="assets/logo.png" style="width: 250px;"/><br/>
+<input placeholder="Type your name" type="text" id="username"/><br/>
+<button class="next" onclick="next()">Next</button>
 `;
 
-function test() {
+function next() {
     name = document.getElementById("username").value;
     appContainer.innerHTML = `
-    <img src="assets/logo.svg" />
-    <button onclick="chooseTeam('Red')">Red</button>
-    <button onclick="chooseTeam('Blue')">Blue</button><br/>
-    <button onclick="play()">Play</button>
+    <img src="assets/logo.png" style="width: 250px;"/><br/>
+    <div>
+        <button onclick="chooseTeam('Red')">Red</button>
+        <button onclick="chooseTeam('Blue')">Blue</button>
+    <div>
+    <button class="next" onclick="play()">Play</button>
     `;
 }
 
