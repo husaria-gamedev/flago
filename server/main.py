0bb81a9f325ec209c03a043cf74395b19f360435
Jan Sierpina <jan.sierpina@solvti.pl>
2023-12-29 14:06:33 +0100

    black

--- server/main.py
+++ server/main.py
@@ -14,20 +14,21 @@ import json
 # GAME
 ########
 
-MAP_WIDTH=1000
-MAP_HEIGHT=500
+MAP_WIDTH = 1000
+MAP_HEIGHT = 500
 BASE_SIZE = 175
 
-PLAYER_RADIUS=10
-PLAYER_SPEED=100 # px/sec
-DEATH_COOLDOWN_SECONDS=10
+PLAYER_RADIUS = 10
+PLAYER_SPEED = 100  # px/sec
+DEATH_COOLDOWN_SECONDS = 10
 
-TICS_PER_SECOND=30
+TICS_PER_SECOND = 30
 
 TEAM_RED = "Red"
 TEAM_BLUE = "Blue"
 
-app = Flask(__name__, static_url_path='', static_folder='../client')
+app = Flask(__name__, static_url_path="", static_folder="../client")
+
 
 @dataclass
 class Player:
@@ -39,6 +40,7 @@ class Player:
     name: str
     direction: float
 
+
 @dataclass
 class State:
     players: list[Player]
@@ -47,6 +49,7 @@ class State:
 
 state = State([], {TEAM_RED: 0, TEAM_BLUE: 0})
 
+
 def game_loop():
     while True:
         for i in range(len(state.players)):
@@ -54,16 +57,19 @@ def game_loop():
 
         for i in range(len(state.players)):
             if is_dead(state.players[i]):
-                state.players[i].died_at = time.time()                
+                state.players[i].died_at = time.time()
                 state.players[i].has_flag = False
 
-            elif state.players[i].died_at != None and (time.time() - state.players[i].died_at) > DEATH_COOLDOWN_SECONDS: 
+            elif (
+                state.players[i].died_at != None
+                and (time.time() - state.players[i].died_at) > DEATH_COOLDOWN_SECONDS
+            ):
                 state.players[i].died_at = None
                 reset_player(state.players[i])
-            
+
         has_flag = {
             TEAM_RED: has_someone_flag(TEAM_RED),
-            TEAM_BLUE: has_someone_flag(TEAM_BLUE)
+            TEAM_BLUE: has_someone_flag(TEAM_BLUE),
         }
 
         for p in state.players:
@@ -80,6 +86,7 @@ def game_loop():
         broadcast_state(state)
         time.sleep(1 / TICS_PER_SECOND)
 
+
 def reset_map():
     for p in state.players:
         reset_player(p)
@@ -91,11 +98,13 @@ def has_someone_flag(team: str) -> bool:
             return True
     return False
 
+
 def move_player(p: Player) -> None:
     distance_per_tick = PLAYER_SPEED / TICS_PER_SECOND
     p.x += math.cos(p.direction) * distance_per_tick
     p.y += math.sin(p.direction) * distance_per_tick
     ensure_player_on_map(p)
+
 
 def ensure_player_on_map(p: Player) -> None:
     if p.x < PLAYER_RADIUS:
@@ -108,12 +117,13 @@ def ensure_player_on_map(p: Player) -> None:
     if p.y > MAP_HEIGHT - PLAYER_RADIUS:
         p.y = MAP_HEIGHT - PLAYER_RADIUS
 
+
 def is_in_base(base: str, p: Player) -> bool:
     if p.y < (MAP_HEIGHT - BASE_SIZE) / 2:
         return False
     if p.y > (MAP_HEIGHT + BASE_SIZE) / 2:
         return False
-    
+
     if base == TEAM_BLUE and p.x < MAP_WIDTH - BASE_SIZE / 2:
         return False
 
@@ -121,6 +131,7 @@ def is_in_base(base: str, p: Player) -> bool:
         return False
 
     return True
+
 
 def is_on_enemy_ground(p: Player) -> bool:
     if is_in_base(other_team(p.team), p):
@@ -133,8 +144,9 @@ def is_on_enemy_ground(p: Player) -> bool:
         return True
     return False
 
+
 def is_dead(p: Player) -> bool:
-    if (not is_on_enemy_ground(p)):
+    if not is_on_enemy_ground(p):
         return False
     for o in state.players:
         if p.team == o.team or o.died_at != None:
@@ -145,16 +157,23 @@ def is_dead(p: Player) -> bool:
             return True
     return False
 
+
 def add_player(name, team):
-    player = Player(0, 0, None, False, team, name,  0.)
+    player = Player(0, 0, None, False, team, name, 0.0)
     reset_player(player)
     state.players.append(player)
 
+
 def reset_player(p: Player) -> None:
-    p.x = PLAYER_RADIUS + BASE_SIZE if p.team == TEAM_RED else MAP_WIDTH - PLAYER_RADIUS - BASE_SIZE
+    p.x = (
+        PLAYER_RADIUS + BASE_SIZE
+        if p.team == TEAM_RED
+        else MAP_WIDTH - PLAYER_RADIUS - BASE_SIZE
+    )
     p.y = MAP_HEIGHT / 2
     p.has_flag = False
     p.died_at = None
+
 
 def other_team(team: str) -> str:
     return TEAM_RED if team == TEAM_BLUE else TEAM_BLUE
@@ -163,6 +182,7 @@ def other_team(team: str) -> str:
 ########
 # ROUTES
 ########
+
 
 @app.route("/register", methods=["POST"])
 def register() -> Response:
@@ -179,9 +199,10 @@ def register() -> Response:
 
     return jsonify(content)
 
+
 @app.route("/set-direction", methods=["POST"])
 def set_direction() -> Response:
-    player_name = request.args.get('name')
+    player_name = request.args.get("name")
 
     content = request.json
 
@@ -198,20 +219,24 @@ def set_direction() -> Response:
 
 listeners = []
 
+
 def format_sse(data: str, event=None) -> str:
-    msg = f'data: {data}\n\n'
+    msg = f"data: {data}\n\n"
     if event is not None:
-        msg = f'event: {event}\n{msg}'
+        msg = f"event: {event}\n{msg}"
     return msg
 
-@app.route('/events', methods=['GET'])
+
+@app.route("/events", methods=["GET"])
 def listen():
     def stream():
         listeners.append(messages := queue.Queue(maxsize=100))
         while True:
             msg = messages.get()
             yield msg
-    return flask.Response(stream(), mimetype='text/event-stream')
+
+    return flask.Response(stream(), mimetype="text/event-stream")
+
 
 def broadcast_state(state):
     msg = format_sse(data=json.dumps(asdict(state)), event="state")
@@ -222,12 +247,13 @@ def broadcast_state(state):
         except queue.Full:
             del listeners[i]
 
+
 ########
 # APP
 ########
 
- 
+
 if __name__ == "__main__":
     thread = Thread(target=game_loop)
     thread.start()
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0", debug=True)
