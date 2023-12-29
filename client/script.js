76f3e8f64ce8db5fc6c1ec125174a56545cb83c9
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2023-12-29 09:44:01 +0100

    variable name change

--- client/script.js
+++ client/script.js
@@ -1,16 +1,17 @@
-const appContainer = document.getElementById("app");
+const APP_CONTAINER = document.getElementById("app");
+const SERVER_ADDRESS = "http://localhost:5000";
 let posX = 50;
 let posY = 50;
 let name, team, canvas;
-appContainer.innerHTML = `
+APP_CONTAINER.innerHTML = `
 <img src="assets/logo.png" style="width: 250px;"/><br/>
-<input placeholder="Type your name" type="text" id="username"/><br/>
+<input required placeholder="Type your name" type="text" id="username"/><br/>
 <button class="next" onclick="next()">Next</button>
 `;
 
 function next() {
   name = document.getElementById("username").value;
-  appContainer.innerHTML = `
+  APP_CONTAINER.innerHTML = `
     <img src="assets/logo.png" style="width: 350px;"/><br/>
     <div class="flag_container">
         <button class="team" id="redButton" onclick="chooseTeam('Red')">
@@ -34,26 +35,25 @@ function chooseTeam(t) {
 }
 
 function play() {
-  fetch("http://127.0.0.1:5000/register", {
+  fetch(`${SERVER_ADDRESS}/register`, {
     method: "POST",
     body: JSON.stringify({ name, team }),
     headers: { "Content-Type": "application/json" },
   });
   const width = 1000;
   const height = 500;
-  appContainer.innerHTML = `
+  APP_CONTAINER.innerHTML = `
     <canvas id="game" width="${width}px" height="${height}px" style="background: white;">
     
     </canvas>
     `;
   canvas = document.getElementById("game");
-  console.log(canvas);
+
   canvas.addEventListener("mousemove", (event) => {
     let deltaX = event.offsetX - posX;
     let deltaY = event.offsetY - posY;
     let rad = Math.atan2(deltaY, deltaX);
-    console.log(rad)
-    fetch(`http://127.0.0.1:5000/set-direction?name=${name}`, {
+    fetch(`${SERVER_ADDRESS}/set-direction?name=${name}`, {
       method: "POST",
       body: JSON.stringify({ direction: rad }),
       headers: { "Content-Type": "application/json" },
@@ -61,8 +61,13 @@ function play() {
   });
 }
 
-const source = new EventSource("http://localhost:5000/events");
+const source = new EventSource(`${SERVER_ADDRESS}}/events`);
 
 source.onmessage = (event) => {
+
   console.log(event);
 };
+
+function render(state) {
+  
+}
