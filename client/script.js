b96a013c339e1e5bfbe184574d9197d05107518b
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2023-12-29 10:28:02 +0100

    render

--- client/script.js
+++ client/script.js
@@ -50,32 +50,39 @@ function play() {
     `;
 
   canvas = document.getElementById("game");
-  let direction = 0, rad = 0;
+  let direction = 0,
+    rad = 0;
   function handleMouseMove(event) {
     if (rad != direction) {
-      direction = rad
+      direction = rad;
       fetch(`${SERVER_ADDRESS}/set-direction?name=${name}`, {
         method: "POST",
         body: JSON.stringify({ direction }),
         headers: { "Content-Type": "application/json" },
       });
     }
-    
-  
   }
   canvas.addEventListener("mousemove", (event) => {
     let deltaX = event.offsetX - posX;
     let deltaY = event.offsetY - posY;
     rad = Math.atan2(deltaY, deltaX);
-
   });
-  setInterval(handleMouseMove, 1000)
+  setInterval(handleMouseMove, 100);
 }
 
 const source = new EventSource(`${SERVER_ADDRESS}/events`);
 
 source.addEventListener("state", (e) => {
+  render(JSON.parse(e.data))
   console.log(JSON.parse(e.data));
 });
 
-function render(state) {}
+function render(state) {
+  const context = canvas.getContext("2d");
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  state.players.forEach(element => {
+    context.rect(element.x, element.y, 10, 10);
+    context.fill();
+  });
+  
+}
